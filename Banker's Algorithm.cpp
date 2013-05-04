3cc42d529b5c9f38cb48cc2339d2875887a6ba77
theGeeksHub <girish@hotmail.co.in>
2013-05-04 09:03:09 +0530

    Commented out - Test data

--- Banker's Algorithm.cpp
+++ Banker's Algorithm.cpp
@@ -28,41 +28,45 @@ int main(int argc, char *argv[])
         for(j = 0; j < typesOfResources; j++)
             ::allocation[i][j] = rand() % 10;
     }
-    ::allocation[0][0] = 0;
-    ::allocation[0][1] = 1;
-    ::allocation[0][2] = 0;
-    ::allocation[1][0] = 2;
-    ::allocation[1][1] = 0;
-    ::allocation[1][2] = 0;
-    ::allocation[2][0] = 3;
-    ::allocation[2][1] = 0;
-    ::allocation[2][2] = 2;
-    ::allocation[3][0] = 2;
-    ::allocation[3][1] = 1;
-    ::allocation[3][2] = 1;
-    ::allocation[4][0] = 0;
-    ::allocation[4][1] = 0;
-    ::allocation[4][2] = 2;
+    /* Test data
+    	::allocation[0][0] = 0;
+    	::allocation[0][1] = 1;
+    	::allocation[0][2] = 0;
+    	::allocation[1][0] = 2;
+    	::allocation[1][1] = 0;
+    	::allocation[1][2] = 0;
+    	::allocation[2][0] = 3;
+    	::allocation[2][1] = 0;
+    	::allocation[2][2] = 2;
+    	::allocation[3][0] = 2;
+    	::allocation[3][1] = 1;
+    	::allocation[3][2] = 1;
+    	::allocation[4][0] = 0;
+    	::allocation[4][1] = 0;
+    	::allocation[4][2] = 2;
+    */
     for(i = 0; i < numberOfProcesses; i++)
     {
       for(j = 0; j < typesOfResources; j++)
             ::max[i][j] = rand() % 10;
     }
-    ::max[0][0] = 7;
-    ::max[0][1] = 5;
-    ::max[0][2] = 3;
-    ::max[1][0] = 3;
-    ::max[1][1] = 2;
-    ::max[1][2] = 2;
-    ::max[2][0] = 9;
-    ::max[2][1] = 0;
-    ::max[2][2] = 2;
-    ::max[3][0] = 2;
-    ::max[3][1] = 2;
-    ::max[3][2] = 2;
-    ::max[4][0] = 4;
-    ::max[4][1] = 3;
-    ::max[4][2] = 3;
+    /* Test data
+	::max[0][0] = 7;
+	::max[0][1] = 5;
+	::max[0][2] = 3;
+	::max[1][0] = 3;
+	::max[1][1] = 2;
+	::max[1][2] = 2;
+	::max[2][0] = 9;
+	::max[2][1] = 0;
+	::max[2][2] = 2;
+	::max[3][0] = 2;
+	::max[3][1] = 2;
+	::max[3][2] = 2;
+	::max[4][0] = 4;
+	::max[4][1] = 3;
+    	::max[4][2] = 3;
+    */
     for(i = 0; i < numberOfProcesses; i++)
     {
 	    for(j = 0; j < typesOfResources; j++)
@@ -71,21 +75,23 @@ int main(int argc, char *argv[])
                 ::need[i][j] = ::max[i][j] - ::allocation[i][j];
 	    }
     }
-    ::need[0][0] = 7;
-    ::need[0][1] = 4;
-    ::need[0][2] = 3;
-    ::need[1][0] = 1;
-    ::need[1][1] = 2;
-    ::need[1][2] = 2;
-    ::need[2][0] = 6;
-    ::need[2][1] = 0;
-    ::need[2][2] = 0;
-    ::need[3][0] = 0;
-    ::need[3][1] = 1;
-    ::need[3][2] = 1;
-    ::need[4][0] = 4;
-    ::need[4][1] = 3;
-    ::need[4][2] = 1;
+    /* Test data
+    	::need[0][0] = 7;
+    	::need[0][1] = 4;
+    	::need[0][2] = 3;
+    	::need[1][0] = 1;
+    	::need[1][1] = 2;
+    	::need[1][2] = 2;
+    	::need[2][0] = 6;
+    	::need[2][1] = 0;
+    	::need[2][2] = 0;
+    	::need[3][0] = 0;
+    	::need[3][1] = 1;
+    	::need[3][2] = 1;
+    	::need[4][0] = 4;
+    	::need[4][1] = 3;
+    	::need[4][2] = 1;
+    */
     cout<<"\n###########################################################";
     cout<<"\nProblem: Safety & Banker's algorithms";
     cout<<"\nNo. of processes: "<<numberOfProcesses;
@@ -173,9 +179,11 @@ void stateCheck()
         ::requestProcess = 1;
         cout<<"\n\tProcess: "<<::requestProcess;
         cout<<"\n\tRequested resources: (";
-        ::requestResources[0] = 1;
-        ::requestResources[1] = 0;
-        ::requestResources[2] = 2;
+        /* Test data
+        	::requestResources[0] = 1;
+        	::requestResources[1] = 0;
+        	::requestResources[2] = 2;
+        */
         for(i = 0; i < typesOfResources; i++)
         {
             cout<<::requestResources[i];
